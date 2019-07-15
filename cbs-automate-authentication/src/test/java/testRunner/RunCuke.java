57c6afb8e202f6b1166c2263e4e69e354735e94c
samson.oluwashola <samson.oluwashola-cic-uk@uk.ibm.com>
2019-07-16 00:01:40 +0100

    added cucumberJVM plugin

--- cbs-automate-authentication/src/test/java/testRunner/RunCuke.java
+++ cbs-automate-authentication/src/test/java/testRunner/RunCuke.java
@@ -5,7 +5,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
  
 
 @CucumberOptions(features = "classpath:featureFile",
-glue = "classpath:stepsDefinitions"
+glue = "classpath:stepsDefinitions",
+plugin = {"pretty", "html:target/cucumber-html-report",
+"json:target/cucumber.json"}
 )
 public class RunCuke extends AbstractTestNGCucumberTests{
 	
